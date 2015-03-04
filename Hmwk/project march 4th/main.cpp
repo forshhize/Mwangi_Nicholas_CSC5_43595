372174404b5bc7c28312099913b2e3210ec8e3fd
forshhize <forshhize@gmail.com>
2015-03-04 10:49:33 -0800

    Signed-off-by: forshhize <forshhize@gmail.com>

---
+++ Hmwk/project march 4th/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 4, 2015, 10:41 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+//
+
+using namespace std;
+const float PI = 3.14159265;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+//
+    return 0;
+}
+
