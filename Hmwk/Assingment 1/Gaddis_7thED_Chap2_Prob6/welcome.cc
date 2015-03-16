9c4e1e6ef1b3e207e450e4e3d0886179003a76d0
forshhize <forshhize@gmail.com>
2015-03-15 20:30:06 -0700

    Signed-off-by: forshhize <forshhize@gmail.com>

---
+++ Hmwk/Assingment 1/Gaddis_7thED_Chap2_Prob6/welcome.cc
@@ -0,0 +1,49 @@
+/*
+ *File: welcome.cc
+ * Author: Nicholas Mwangi
+ * 
+ * Created on March 15, 2015, 8:12 PM
+ * Purpose: Annual Pay
+ */
+
+//System Libraries
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+//Global Constants
+//Execution begins here!!
+
+int main(int argc, char**argv) {
+    
+    
+    
+    
+    unsigned short payAmt,payPrds,annlPay;
+    
+    payAmt= 1700.0;
+    
+    payPrds= 26;
+    
+    annlPay;
+    
+    annlPay= payAmt * payPrds;
+            
+            
+    
+    
+    
+    //calculate annual pay
+    cout<< "The annual pay is $ " << annlPay << "." << endl;
+    
+   
+    
+    
+            
+            
+            
+    
+    return 0;
+}
