02f74ee2030c4a3836c0629187922df14d52511a
forshhize <forshhize@gmail.com>
2015-03-16 09:26:24 -0700

    Signed-off-by: forshhize <forshhize@gmail.com>

---
+++ Lab/ifelse/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=ifelse
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/ifelse
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=ifelse.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/ifelse.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=ifelse
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/ifelse
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=ifelse.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/ifelse.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ Lab/ifstatements/main.cpp
@@ -0,0 +1,67 @@
+/* 
+ * File:   main.cpp
+ * Author: Nicholas Mwangi
+ * Purpose:paycheck-Ternary
+ *
+ * Created on March 16, 2015, 8:11 AM
+ */
+//System Libraries
+#include <iostream>
+
+#include <iomanip>//Formatting
+#include <fstream>//I/O File
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Types
+
+//Execution starts here!
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    //Declare variables
+    ofstream output;//output the results in a file
+    float hrsWkd;//Hours Worked (Hrs)
+    float payRate;//Pay Rate ($'s/hr)
+    float oRate;//multiplicative factor of payrate
+    float oTime;//Where overtime begins (hr)
+    float payChk;//Gross pay($'s)
+    //open the file
+    output.open("payroll.dat");
+    //prompt the user for inputs
+    cout<<"How many hours this week did you work?" <<endl;
+    cin>>hrsWkd;
+    cout<<"What is your pay rate ($'s/hr)"<<endl;
+    cin>>payRate;
+    //calculate the paycheck
+    if(hrsWkd<oTime){
+        payChk=payRate*hrsWkd;
+    }
+    if(hrsWkd<oTime){
+        payChk=payRate*(hrsWkd+(oRate-1)*(hrsWkd-oTime));
+    }
+    //output the results on your screen
+    cout<<fixed<<setprecision (2) <<showpoint;
+    cout<<"Hours worked="hrsWkd<<"(hrs)"<<endl;
+    cout<<"pay Rate=$"<<payRate<<"/(hr)"<<endl;
+    cout<<"Over time rate="<<oRate<<endl;
+    cout<<"Overtime bgins at"<<oTime<<"(hrs)"<<endl;
+    cout<<"Gross pay =$"<<payChk<<endl;
+            
+         
+            
+           
+    //close he file
+    output.close();
+    
+    
+
+    return 0;
+}
