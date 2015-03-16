a87c4b4a26925c4ca812b593398485e687e17a78
forshhize <forshhize@gmail.com>
2015-03-15 18:17:33 -0700

    Signed-off-by: forshhize <forshhize@gmail.com>

---
+++ Hmwk/Assingment 1/Gaddis_7thED_Chap2_Prob10/welcome.cc
@@ -0,0 +1,39 @@
+/*
+ * File:   Welcome.cpp
+ * Author: Nicholas Mwangi
+ *
+ * Created on March 15, 2015, 5:30 PM
+ * Purpose: To calculate selling price of a circuit Board at a 40% profit. 
+ * 
+ */
+//System Libraries
+
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Execution begins Here!!
+
+int main(int argc, char**argv) {
+    
+    float maxGall, maxDist, mpg;
+    
+    
+    maxGall = 12;
+    
+    maxDist = 350;
+    
+    mpg = maxDist / maxGall;
+    
+    cout<< "The number of miles the car gets per gallon is " << mpg << " miles" << endl;
+            
+            
+    
+    
+    return 0;
+}
