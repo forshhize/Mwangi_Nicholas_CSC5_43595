692c8a1f51c75f0963d303cb57413f4503f7c1da
forshhize <forshhize@gmail.com>
2015-03-25 10:58:20 -0700

    Signed-off-by: forshhize <forshhize@gmail.com>

---
+++ Hmwk/Assignment 3/Gaddis_7thED_Chap4_Prob1/welcome.cc
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.cpp
+ * Author: Nicholas Mwangi
+ * Created on March 18, 2015, 11:27  AM
+ *      Purpose: Minimum and Maximum
+ */
+
+//System Libraries
+
+#include <iostream>//I/O standard
+#include <iomanip>//Formatting
+#include <fstream>//I/O File
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+ 
+int main(int argc, char**argv) {
+    // Declare variables
+    
+    float nmber1,nmber2;
+   
+    //Prompt the user for inputs
+    
+    cout<< "Enter the first number?"<<endl;
+    cin>> nmber1;
+    
+    cout<< "Enter the second number?"<<endl;
+    cin>> nmber2;
+    
+    if (nmber1 > nmber2)
+        cout<< "The first number is greater than  "<< nmber2 << endl;
+    else if (nmber2 > nmber1)
+        cout<< "The second number is greater than  "<< nmber1 << endl;
+    else  
+        cout<< "nmber1=nmber2" <<endl;
+    
+    
+    
+    
+    
+   
+    return 0;
+}
