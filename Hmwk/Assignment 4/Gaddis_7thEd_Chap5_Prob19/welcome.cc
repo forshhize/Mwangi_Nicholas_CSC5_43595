0d97f6741bfcba10c1dd11db5ae9e7d671be40d1
forshhize <forshhize@gmail.com>
2015-04-16 16:16:04 -0700

    Signed-off-by: forshhize <forshhize@gmail.com>

--- Hmwk/Assignment 4/Gaddis_7thEd_Chap5_Prob19/welcome.cc
+++ Hmwk/Assignment 4/Gaddis_7thEd_Chap5_Prob19/welcome.cc
@@ -1,46 +1,57 @@
-/*
- * Copyright (c) 2009-2010, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * * Neither the name of Oracle nor the names of its contributors
- *   may be used to endorse or promote products derived from this software without
- *   specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+/* 
+ * File:   main.cpp
+ * Author: Nicholas Mwangi
+ * Created on April 13, 2015, 11:27  AM
+ * Purpose: Budget Analysis
  */
 
-#include <iostream>
+//System Libraries
 
+#include <iostream>//I/O standard
+
+
+
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+ 
 int main(int argc, char**argv) {
-    // Prints welcome message...
-    std::cout << "Welcome ..." << std::endl;
-
-    // Prints arguments...
-    if (argc > 1) {
-        std::cout << std::endl << "Arguments:" << std::endl;
-        for (int i = 1; i < argc; i++) {
-            std::cout << i << ": " << argv[i] << std::endl;
-        }
-    }
+    // Declare variables
+    
+    float budget,expense;
+    float total=0; //accumaltor to keep a running total
+    
+    //inputting budget
+    
+    cout<< "Enter what you have budgeted for the month:";
+    cin>>budget;
+    
+    cout<<"Enter all expenses:" ;
+     cin >>expense;
+    
+    // utilize a do-while since its a post test to see how much is spent first
+    do
+    { 
+     
+        total=total + expense;
+        
+    }while (expense=0);
+    
+    if (total<=budget)
+    
+        cout<< "Expenses are under budget" <<endl;
+    
+    else
+        cout<<"Expenses are over budget" <<endl;
+    
+    
     
     return 0;
 }
